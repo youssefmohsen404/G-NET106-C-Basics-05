c716f95df267fb6d531caa35eebe1c7fa69b4dc8
youss <youssmohss111@gmail.com>
2026-09-09 01:35:14 +0300

    Q1

---
+++ G-NET106-C#Basics-05/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET106_C_Basics_05
+{
+    internal class Book
+    {
+
+       // private  string password = "secret";
+    
+    }
+}
--- G-NET106-C#Basics-05/Program.cs
+++ G-NET106-C#Basics-05/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
-        }
+            #region Question 1
+            //Add a private string password = "secret"; field to a Book class. Try to print it from Main
+            //(outside the class). What happens, and why?
+            /*Book book = new Book();
+            book.password; 
+*/
+            //it gives a compile time error cause the access modifier is private for the object 
+            //so i can reach the object only inside the class i have to make it internal 
+            #endregion
+    }
     }
 }
